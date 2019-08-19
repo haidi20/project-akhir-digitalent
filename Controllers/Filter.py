16b36b3ca27276f3eb57ff1f8616e324b51e93ec
haidi20 <nhaidii@yahoo.com>
2019-08-20 02:55:03 +0800

    update-filter

--- Controllers/Filter.py
+++ Controllers/Filter.py
@@ -1,3 +1,30 @@
+import re
+
+import pandas as pd
+
+from flask import jsonify 
 class Filter:
     def index():
-        return 'filter'
+        tweets = pd.read_csv('./sentiment analysis/data/stream_tweet.csv')
+        tweets = tweets['text']
+
+        data = []
+        for tweet in tweets:
+            data.append(Filter.clean_text(tweet))
+        
+        return jsonify(data)
+
+    def clean_text(text):
+        text = text.translate({ord(i): None for i in '@'})
+        text = text.translate({ord(i): None for i in '#'})
+        text = text.translate({ord(i): None for i in 'RT'})
+        text = text.lower()
+        text = re.sub(r'\W+', ' ', text)
+        text = re.sub(r'\s+', ' ', text)
+        text = re.sub(r"\\", "", text)
+        text = re.sub(r"\'", "", text)    
+        text = re.sub(r"\"", "", text)
+        text = re.sub("com", "", text)
+        return text
+            
+        
