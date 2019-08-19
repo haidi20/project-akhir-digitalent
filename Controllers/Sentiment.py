02a66c23d55831b4e035f33fc0c360556b4be5a8
haidi20 <nhaidii@yahoo.com>
2019-08-20 04:19:28 +0800

    data-bukalapak

--- Controllers/Sentiment.py
+++ Controllers/Sentiment.py
@@ -1,5 +1,45 @@
+import seaborn as sns 
+
+from flask import jsonify 
+
+from Controllers.Cloud_watch import Cloud_watch as cloud_watch
 
 class Sentiment:
 
     def index():
-        return 'sentiment'
+        data = cloud_watch.index()
+
+        data = Sentiment.anl_tweets(data)
+
+        return jsonify(data)
+
+    def sentiment_analyzer_scores(text, engl=True):
+        if engl:
+            trans = text
+        else:
+            trans = translator.translate(text).text
+        score = analyser.polarity_scores(trans)
+        lb = score['compound']
+        if lb >= 0.05:
+            return 1
+        elif (lb > -0.05) and (lb < 0.05):
+            return 0
+        else:
+            return -1
+
+    def anl_tweets(lst, title='Tweets Sentiment', engl=True ):
+        sents = []
+        for tw in lst:
+            try:
+                st = Sentiment.sentiment_analyzer_scores(tw, engl)
+                sents.append(st)
+            except:
+                sents.append(0)
+        ax = sns.distplot(
+            sents,
+            kde=False,
+            bins=3)
+        ax.set(xlabel='Negative                Neutral                 Positive',
+            ylabel='#Tweets',
+            title="Tweets of @"+title)
+        return sents
